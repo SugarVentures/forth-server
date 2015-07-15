e9cccd2fd4917fda14b374b8047d4aa1a5aa3791
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-07-15 15:39:31 +0800

    change validation to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :email, presence: true, allow_blank: false, uniqueness: { scope: [:deleted_at] }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, allow_blank: false, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true, allow_blank: false, uniqueness: true, length: { maximum: 50, too_long: '%{count} characters is the maximum allowed' }
 end
