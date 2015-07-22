4276236f5d644415129737673f9d67acc1fff70f
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-07-22 16:11:54 +0800

    add min_age to User Model

--- db/migrate/20150715054056_add_name_to_users.rb
+++ db/migrate/20150715054056_add_name_to_users.rb
@@ -2,5 +2,6 @@ class AddNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string, default: "", null: false, uniq: true
     add_column :users, :birthday, :datetime
+    add_column :users, :min_age, :integer
   end
 end
