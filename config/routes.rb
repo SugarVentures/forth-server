8335d8a6e98d55c8009203c754f5d370204df76e
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-03 15:19:35 +0800

    add users/confirmations path to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :omniauth_callbacks => "users/omniauth_callbacks" 
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    confirmations: 'users/confirmations'
   }
 
   root to: "forth#index"
