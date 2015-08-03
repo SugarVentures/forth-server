a396c8a88e60404aadcb948cb5c4387da33681ec
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-03 16:41:06 +0800

    add testing for User Model to RSpec User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_uniqueness_of(:email) }
+
+  it 'should have valid factory' do
+    FactoryGirl.build(:user).should be_valid
+  end
+
+  it 'should require an email' do
+    FactoryGirl.build(:user, email: '').should_not be_valid
+  end
 end
