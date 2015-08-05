7131918962d2638676895c1f4daae29711087378
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-05 11:38:21 +0800

    add Channel Model

---
+++ app/models/channel.rb
@@ -0,0 +1,8 @@
+class Channel < ActiveRecord::Base
+  validates :title, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
+
+  enum components: [:live, :upcomming, :past]
+end
