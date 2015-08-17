179b8413e661a80cb979b7cd82381c14ac50e470
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-17 14:21:07 +0800

    add testing for Channel Model to RSpec Channel Model

--- spec/factories/channel.rb
+++ spec/factories/channel.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :channel do
     title { Faker::Lorem.characters(10) }
+    user
   end
 end
--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Channel, type: :model do
   it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of :user_id }
 
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:streams) }
 
-  it { is_expected.to validate_presence_of :user_id }
+  let(:channel) { create :channel }
+
+  it 'acts as paranoid' do
+    channel.delete
+    expect(channel.deleted_at).not_to be_nil
+    expect(Channel.only_deleted).to include(channel)
+  end
 end
