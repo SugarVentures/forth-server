e59b595b4841bd35c65ebdfff873dfd10acda7da
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-17 14:30:37 +0800

    add testing for Stream model to RSpec Stream Model

--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Stream, type: :model do
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :stream_key }
+  it { is_expected.to validate_uniqueness_of(:stream_key) }
+  it { is_expected.to validate_presence_of :age_restriction }
+  it { is_expected.to validate_presence_of :user_id }
+  it { is_expected.to validate_presence_of :channel_id }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:channel) }
 
-  it { is_expected.to validate_uniqueness_of(:stream_key) }
+  let(:stream) { create :stream }
 
-  it { is_expected.to validate_presence_of :user_id }
-  it { is_expected.to validate_presence_of :channel_id }
+  it 'acts as paranoid' do
+    stream.delete
+    expect(stream.deleted_at).not_to be_nil
+    expect(Stream.only_deleted).to include(stream)
+  end
 end
