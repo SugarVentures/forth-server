8999a1d17e9416b2cf3fd194c45b4f6f2f37ae5d
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-17 18:27:42 +0800

    add test for Streams Controller to RSpec Streams Controller

---
+++ spec/controllers/streams_controller_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+include Devise::TestHelpers
+
+RSpec.describe StreamsController, type: :controller do
+  let!(:user) { create :user }
+  let!(:channel) { create :channel, user: user }
+  let!(:stream) { create :stream, user: user, channel: channel }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    it 'returns http success for sign_in user' do
+      get :index, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:streams]).to include(stream)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success without sign_in' do
+      sign_out user
+      get :index, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:streams]).to include(stream)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:stream]).to be_kind_of(Stream)
+      expect(assigns[:stream].id).to be_nil
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success for sign_in user' do
+      get :show, id: stream.id, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:stream]).to eq(stream)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success without sign_in' do
+      sign_out user
+      get :show, id: stream.id, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:stream]).to eq(stream)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'returns 302' do
+      params = { channel_id: channel.id, stream: { title: 'abc', game: 'game1' } }
+      post :create, params
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:stream].title).to eq('abc')
+      expect(Stream.find(assigns[:stream].id).game).to eq('game1')
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, id: stream.id, channel_id: channel.id
+      expect(assigns[:channel]).to eq(channel)
+      expect(assigns[:stream]).to eq(stream)
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
