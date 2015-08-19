103f611f52fdf062a41de530ecef18bbf33bd60e
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-08-19 15:57:38 +0800

    change client_key to chef-repo/.chef/knife.rb

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -4,7 +4,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "shinya14"
-client_key               "#{ENV['HOME']}/.chef/shinya.pem"
+client_key               "#{ENV['HOME']}/.chef/shinya14.pem"
 validation_client_name   "forth-validator"
 validation_key           "#{ENV['HOME']}/.chef/forth-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/forth"
@@ -14,5 +14,5 @@ cookbook_path            ["#{current_dir}/../cookbooks"]
 
 knife[:aws_credential_file] = "#{ENV['HOME']}/.chef/jf2.aws.credentials"
 knife[:region] = 'ap-southeast-1'
-knife[:editor] = 'emacs'
+knife[:editor] = 'vim'
 knife[:secret_file] = '/etc/chef/forth_secret'
