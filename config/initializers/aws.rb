f56ba663b9a0502d93665d2f80f647fc4ecf2fea
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-01 16:56:26 +0800

    Fix config/initializers/aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-AWS.config(Rails.application.secrets['aws']) if Rails.application.secrets['aws']
+AWS.config(Rails.application.secrets.aws) if Rails.application.secrets.aws
 
 module Net
   class SMTP
