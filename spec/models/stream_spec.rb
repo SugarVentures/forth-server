7481417fecdc0980f5a020b3a4a7d202abcf4d92
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-09 23:50:07 +0800

    Add fixture_file_upload to RSpec Stream Model

--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe Stream, type: :model do
   it { is_expected.to belong_to(:channel) }
 
   let(:stream) { create :stream }
-  let(:file_path) { "#{Rails.root}/spec/fixtures/images/lego.png" }
-  let(:encoded_file) { Base64.encode64(file_path) }
 
   it 'acts as paranoid' do
     stream.delete
@@ -22,14 +20,14 @@ RSpec.describe Stream, type: :model do
 
   it 'is valid to have less than limitation number of videos' do
     Video::MAX_UPLOAD.times do
-      stream.videos.create(file: encoded_file)
+      stream.videos.create(file: fixture_file_upload('images/smile.png', 'image/png'))
     end
     expect(stream.valid?).to eq(true)
   end
 
   it 'is invalid to have more than limitation number of videos' do
     (Video::MAX_UPLOAD + 1).times do
-      stream.videos.create(file: encoded_file)
+      stream.videos.create(file: fixture_file_upload('images/smile.png', 'image/png'))
     end
     expect(stream.valid?).to eq(false)
     expect(stream.errors.full_messages.first).to include('Only 5 videos can be uploaded')
