ef77adead89d3af3c53747d930cb881a2f78b867
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-15 17:49:37 +0800

    Add processing under test to Carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
-    config.enable_processing = false
+    # config.enable_processing = false
 
     if Rails.env.test?
       ImageUploader
