5d3d08cbb898ef2f708c0e1010753dc9bf17ae18
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-15 17:48:15 +0800

    Add thumbnail to Video Uploader

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -2,10 +2,24 @@
 
 class VideoUploader < CarrierWave::Uploader::Base
   include CarrierWave::Video
+  include CarrierWave::Video::Thumbnailer
+
+  def extension_white_list
+    %w(webm mp4 png jpg)
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # process encode_video: [:mp4, callbacks: { after_transcode: :set_success } ]
+  version :thumb do
+    process thumbnail: [{ format: 'png', quality: 10, size: 192, strip: true, logger: Rails.logger }]
+    def full_filename(for_file)
+      png_name for_file, version_name
+    end
+  end
+
+  def png_name(for_file, version_name)
+    %(#{version_name}_#{for_file.chomp(File.extname(for_file))}.png)
+  end
 end
