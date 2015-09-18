a6c5b3eef4cd5d7c41c77faad13d664275b2cb94
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-18 16:08:42 +0800

    Add pagination to Search result page

--- app/controllers/forth_controller.rb
+++ app/controllers/forth_controller.rb
@@ -17,5 +17,6 @@ class ForthController < ApplicationController
 
   def search
     @results = SearchService.new(params[:q]).run
+    @results = Kaminari.paginate_array(@results).page(params[:page]).per(3)
   end
 end
