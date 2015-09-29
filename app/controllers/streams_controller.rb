6490813addd1cc45bba2f4e51b4062d32aa7e091
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-09-29 17:17:01 +0800

    Add temporary key to Streams Controller new

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -5,32 +5,25 @@ class StreamsController < ApplicationController
   before_action :authenticate_user_from_token!, if: :format_json?
 
   def index
-    @streams = @channel.streams.includes(:user)
+    @streams = @channel.streams.where(temp: false).includes(:user)
     @streams = streams_scope
   end
 
   def new
-    @stream = Stream.new
+    delete_temps if @channel.streams.where(temp: true).count > 1
+    @stream = @channel.streams.find_by(temp: true)
+    @stream = @channel.streams.create(new_stream) if @stream.nil?
   end
 
   def show
-  end
-
-  def create
-    @stream = new_stream
-    if @stream.save
-      redirect_to [@channel, @stream], notice: 'Stream was successfully created.'
-    else
-      render :new
-    end
   end
 
   def edit
   end
 
   def update
-    if @stream.update(stream_params)
-      redirect_to [@channel, @stream], notice: 'Stream was successfully updated.'
+    if @stream.update stream_params.merge(temp: false)
+      redirect_to [@channel, @stream], notice: 'Stream was successfully created.'
     else
       render :edit
     end
@@ -76,13 +69,18 @@ class StreamsController < ApplicationController
     @stream = Stream.find(params[:id])
   end
 
+  def delete_temps
+    s_ids = @channel.streams.where(temp: true).pluck(:id)
+    Stream.where(id: s_ids).delete_all
+    Stream.only_deleted.where(id: s_ids).delete_all
+  end
+
   def new_stream
-    s_params = stream_params.merge(stream_key: SecureRandom.uuid)
-    @channel.streams.new(s_params)
+    { title: current_user.name + ' stream', user: current_user, stream_key: SecureRandom.uuid, temp: true }
   end
 
   def stream_params
-    params.require(:stream).permit(:title, :game, :start, :end, :view_mode, :age_restriction, :group, :discussion, :description, :image).merge(user: current_user).tap do |p|
+    params.require(:stream).permit(:title, :game, :start, :end, :stream_key, :view_mode, :age_restriction, :group, :discussion, :description, :image).merge(user: current_user).tap do |p|
       p[:view_mode] = p[:view_mode].to_i if p[:view_mode]
     end
   end
