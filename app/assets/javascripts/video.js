2249152df7a22485af94ee5a44b1b216044917d4
vanpt <vanpt>
2015-10-01 17:14:43 +0700

    fixed preview video js

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $("#video_file").change(function(){
         $('#videoPreviewPlayer').show();
-            var myPlayer = videojs('videoPreviewPlayer').ready(function () {
+        videojs('videoPreviewPlayer', {}, function() {
             var filename = $('#video_file').get(0).files[0].name;
             var fileUrl = URL.createObjectURL($('#video_file').get(0).files[0]);
             var fileType = $('#video_file').get(0).files[0].type;
-            //console.log(filename);
+            console.log(filename);
             this.src({ type: fileType, src: fileUrl });
             this.load();
             this.play();
