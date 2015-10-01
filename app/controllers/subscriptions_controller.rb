6e631ce783c5b646ff2feba96a6721239d5bb83e
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-10-01 10:42:45 +0800

    Add subscriptions index page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,10 @@
 class SubscriptionsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_channel, except: :index
+  before_action :authenticate_user!, except: :index
+  before_action :set_channel, only: [:create, :destroy]
+  before_action :set_user, only: :index
 
   def index
-    @subscriptions = current_user.all_following
+    @subscriptions = @user.all_following
   end
 
   def create
@@ -18,5 +19,9 @@ class SubscriptionsController < ApplicationController
 
   def set_channel
     @channel = Channel.find(params[:channel_id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
