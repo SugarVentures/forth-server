6f41f4482dc5c65f27cc3d1fda614f6560f24ebb
Shinya Kitamura <shinya.kitamura@sugar.sg>
2015-10-01 11:19:06 +0800

    Add Spec Subscriptions_controller

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe SubscriptionsController, type: :controller do
 
   before do
     sign_in user
+    user.follow(user.channel)
   end
 
-  describe "GET #index" do
-    it "returns http success" do
-      # get :index
-      # expect(response).to have_http_status(:success)
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index, user_id: user.id
+      expect(assigns[:subscriptions]).to be_kind_of(Array)
+      expect(assigns[:subscriptions].first).to eq(user.channel)
+      expect(response).to have_http_status(:success)
     end
   end
-
 end
