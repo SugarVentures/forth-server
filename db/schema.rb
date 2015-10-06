5bae8e105fe89fe3ca295d7db97c7ad79b1275f7
vanpt <vanpt>
2015-10-06 17:47:27 +0700

    added subscriptions page

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20151005092650) do
     t.datetime "updated_at",                               null: false
     t.string   "name",                     default: "",    null: false
     t.datetime "birthday"
-    t.integer  "min_age"
     t.string   "fb_id"
     t.string   "fabric_id"
     t.string   "fabric_auth_token"
